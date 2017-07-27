fd67daa7c911736efd35171ca64725e7257b816a
brenopolanski <breno.polanski@gmail.com>
2017-07-27 11:43:11 -0300

    Add initial code for the server

--- books-api/server.js
+++ books-api/server.js
@@ -1,3 +1,37 @@
 require('dotenv').config();
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const config = require('config');
+const books = require('books');
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+}
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  const help = `
+  <pre>
+    Welcome to the Book Lender API!
+
+    Use an Authorization header to work with your own data:
+
+    fetch(url, { headers: { 'Authorization': 'whatever-you-want' }})
+
+    The following endpoints are available:
+
+    GET /books
+    GET /books/:id
+    PUT /books/:id { shelf }
+    POST /search { query, maxResults }
+  </pre>
+  `;
+
+  res.send(help);
+});
